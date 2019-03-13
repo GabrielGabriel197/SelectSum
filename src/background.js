c5504f14a29a369e07e5994e38dcdea997bc44d2
GabrielGabriel197 <gabrielgabriel197@gmail.com>
2019-03-13 19:50:46 -0300

    Removed the main Sum function from the install listener, and added manifest.json

--- src/background.js
+++ src/background.js
@@ -1,23 +1,19 @@
 let Sum = [];
 
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        chrome.contextMenus.create({
-            "title" : "Add to sum",
-            "id"    : "AddSum",
-            contexts: ["selection"],
-        });
-        
-        chrome.contextMenus.onClicked.addListener(function(info, tab){
-            if (info.menuItemId == "AddSum"){
-                let Text = info.selectionText;
+chrome.contextMenus.create({
+    "title" : "Add to sum",
+    "id"    : "AddSum",
+    contexts: ["selection"],
+});
 
-                if(!isNaN(Text)){
-                    Sum.push(parseFloat(Text));
-                    chrome.extension.getBackgroundPage().console.log(Sum);
-                    // isn't working for some reason
-                }
-            }
-        });
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+    if (info.menuItemId == "AddSum"){
+        let Text = info.selectionText;
+
+        if(!isNaN(Text)){
+            Sum.push(parseFloat(Text));
+            chrome.extension.getBackgroundPage().console.log(Sum);
+            // isn't working for some reason
+        }
     }
 });
